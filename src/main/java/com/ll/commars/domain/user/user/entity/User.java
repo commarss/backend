5ba03b3789f4e76d339b8d9655400f82f1fc110c
whqtker <chosh2001@naver.com>
2025-02-04 10:38:07 +0900

    fix: user entity 수정 완료

--- src/main/java/com/ll/commars/domain/user/user/entity/User.java
+++ src/main/java/com/ll/commars/domain/user/user/entity/User.java
@@ -1,26 +1,48 @@
 package com.ll.commars.domain.user.user.entity;
 
+import com.ll.commars.global.baseEntity.BaseEntity;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "users")
 @Getter
 @Setter
-@Table(name = "user")
-public class User {
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int userId;
+    private Long id;
 
-    @Column(nullable = false, unique = true)
+    // 1: 카카오, 2: 네이버, 3: 구글
+    @NotNull
+    @Column(name = "social_provider", nullable = false)
+    private Integer socialProvider;
+
+    @Column(name = "email", nullable = false)
     private String email;
-    private String gender;
 
-    @Column(nullable = false)
+    @Column(name = "name")
     private String name;
 
-    @Column(nullable = false)
+    @Column(name = "password")
     private String password;
 
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "profile_image_url")
+    private String profileImageUrl;
+
+    @Column(name = "birth_date")
+    private LocalDateTime birthDate;
+
+    // 1: 남성, 2: 여성
+    @Column(name = "gender")
+    private Integer gender;
 }
