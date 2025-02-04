06dc2a715269881fb5b7beb582a9fb32b5845f17
whqtker <chosh2001@naver.com>
2025-02-04 10:02:19 +0900

    feat: baseEntity 추가

---
+++ src/main/java/com/ll/commars/global/baseEntity/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.ll.commars.global.baseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+
+// 테이블로 매핑하지 않고 자식 클래스에게 매핑 정보만 제공하는 어노테이션
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
