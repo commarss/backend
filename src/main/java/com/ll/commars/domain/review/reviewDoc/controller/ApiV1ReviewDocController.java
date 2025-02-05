f0c818ebecfade2d2ba0dae1d3883f5a4839e514
whqtker <chosh2001@naver.com>
2025-02-05 11:36:10 +0900

    docs: ApiV1ReviewDocController Swagger 적용 완료

--- src/main/java/com/ll/commars/domain/review/reviewDoc/controller/ApiV1ReviewDocController.java
+++ src/main/java/com/ll/commars/domain/review/reviewDoc/controller/ApiV1ReviewDocController.java
@@ -3,33 +3,38 @@ package com.ll.commars.domain.review.reviewDoc.controller;
 import com.ll.commars.domain.review.reviewDoc.document.ReviewDoc;
 import com.ll.commars.domain.review.reviewDoc.service.ReviewDocService;
 import com.ll.commars.global.rsData.RsData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
-@RequestMapping("/api/v1/reviewsDocs")
+@RequestMapping(value = "/api/v1/reviewsDocs", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
+@Tag(name = "ApiV1ReviewDocController", description = "리뷰 CRUD API(ElasticSearch)")
 public class ApiV1ReviewDocController {
     private final ReviewDocService reviewDocService;
 
-    @PostMapping("/write")
-    public RsData<ReviewDoc> write(
-            @RequestBody @Valid ReviewsDocWriteRequest request
-    ){
-        ReviewDoc reviewDoc = reviewDocService.write(request.content, request.rate);
-        return new RsData<>("201", "리뷰 작성 성공", reviewDoc);
-    }
+//    @PostMapping("/write")
+//    public RsData<ReviewDoc> write(
+//            @RequestBody @Valid ReviewsDocWriteRequest request
+//    ){
+//        ReviewDoc reviewDoc = reviewDocService.write(request.content, request.rate);
+//        return new RsData<>("201", "리뷰 작성 성공", reviewDoc);
+//    }
 
-    record ReviewsDocWriteRequest(
-            @NotBlank String content,
-            @NotBlank Integer rate
-    ) {}
+//    record ReviewsDocWriteRequest(
+//            @NotBlank String content,
+//            @NotBlank Integer rate
+//    ) {}
 
     @GetMapping("/search")
+    @Operation(summary = "리뷰 검색")
     public List<ReviewDoc> search(@RequestParam("keyword") String keyword) {
         return reviewDocService.searchByKeyword(keyword);
     }
