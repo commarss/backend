345242b0c190f38cee3040452bf6657f0e278bda
whqtker <chosh2001@naver.com>
2025-02-05 11:32:31 +0900

    docs: ApiV1ReviewController Swagger 적용 완료

--- src/main/java/com/ll/commars/domain/review/review/controller/ApiV1ReviewController.java
+++ src/main/java/com/ll/commars/domain/review/review/controller/ApiV1ReviewController.java
@@ -4,27 +4,31 @@ import com.ll.commars.domain.review.review.dto.ReviewDto;
 import com.ll.commars.domain.review.review.entity.Review;
 import com.ll.commars.domain.review.review.service.ReviewService;
 import com.ll.commars.global.rsData.RsData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/review")
+@RequestMapping(value = "/api/v1/review", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+@Tag(name = "ApiV1ReviewController", description = "리뷰 CRUD API")
 public class ApiV1ReviewController {
     private final ReviewService reviewService;
 
-    // 모든 리뷰 조회
     @GetMapping("/")
+    @Operation(summary = "리뷰 전체 조회")
     public RsData<ReviewDto.ReviewShowAllResponse> getReviews() {
         ReviewDto.ReviewShowAllResponse response = reviewService.getReviews();
         return new RsData<>("200", "모든 리뷰 조회 성공", response);
     }
 
-    // 특정 리뷰 삭제(완료)
     @DeleteMapping("/{review_id}")
+    @Operation(summary = "리뷰 삭제")
     public RsData<String> deleteReview(
             @PathVariable("review_id") @NotNull Long reviewId
     ){
@@ -32,8 +36,8 @@ public class ApiV1ReviewController {
         return new RsData<>("200", "리뷰 삭제 성공", "리뷰 삭제 성공!");
     }
 
-    // 특정 리뷰 수정
     @PatchMapping("/{review_id}")
+    @Operation(summary = "리뷰 수정")
     public RsData<ReviewDto.ReviewWriteResponse> modifyReview(
             @PathVariable("review_id") @NotNull Long reviewId,
             @RequestBody @Valid ReviewDto.ReviewWriteRequest request
