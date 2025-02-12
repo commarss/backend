7514d8aef5184dbe8d86514a453886895d1e7390
seonghyeok <chosh2001@naver.com>
2025-02-12 15:14:40 +0900

    deploy: test11 timeout 늘림

--- infraScript/zero_downtime_deploy.py
+++ infraScript/zero_downtime_deploy.py
@@ -76,7 +76,7 @@ class ServiceManager:
     def _is_service_up(self, port: int) -> bool:
         url = f"http://127.0.0.1:{port}/actuator/health"
         try:
-            response = requests.get(url, timeout=5)  # 5초 이내 응답 없으면 예외 발생
+            response = requests.get(url, timeout=30)  # n초 이내 응답 없으면 예외 발생
             if response.status_code == 200 and response.json().get('status') == 'UP':
                 return True
         except requests.RequestException:
