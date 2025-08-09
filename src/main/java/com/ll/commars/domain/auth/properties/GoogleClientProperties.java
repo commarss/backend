4ec4ff732762aa21f4a034d4208102b184b66e54
seonghyeok <chosh2001@naver.com>
2025-08-09 20:40:36 +0900

    chore: 프로퍼티에 유효성 로직 추가

--- src/main/java/com/ll/commars/domain/auth/properties/GoogleClientProperties.java
+++ src/main/java/com/ll/commars/domain/auth/properties/GoogleClientProperties.java
@@ -1,13 +1,30 @@
 package com.ll.commars.domain.auth.properties;
 
+import org.hibernate.validator.constraints.URL;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import jakarta.validation.constraints.NotBlank;
 
 @ConfigurationProperties(prefix = "oauth.google")
+@Validated
 public record GoogleClientProperties(
+	@NotBlank
+	@URL
 	String tokenUrl,
+
+	@NotBlank
+	@URL
 	String userInfoUrl,
+
+	@NotBlank
+	@URL
 	String redirectUrl,
+
+	@NotBlank
 	String clientId,
+
+	@NotBlank
 	String clientSecret
 ) {
 }
--- src/main/java/com/ll/commars/domain/auth/properties/KakaoClientProperties.java
+++ src/main/java/com/ll/commars/domain/auth/properties/KakaoClientProperties.java
@@ -1,12 +1,27 @@
 package com.ll.commars.domain.auth.properties;
 
+import org.hibernate.validator.constraints.URL;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import jakarta.validation.constraints.NotBlank;
 
 @ConfigurationProperties(prefix = "oauth.kakao")
+@Validated
 public record KakaoClientProperties(
-		String tokenUrl,
-		String userInfoUrl,
-		String redirectUrl,
-		String clientId
+	@NotBlank
+	@URL
+	String tokenUrl,
+
+	@NotBlank
+	@URL
+	String userInfoUrl,
+
+	@NotBlank
+	@URL
+	String redirectUrl,
+
+	@NotBlank
+	String clientId
 ) {
 }
