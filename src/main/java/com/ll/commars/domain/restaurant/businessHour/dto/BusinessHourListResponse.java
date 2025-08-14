562e761fc50740d921debcfd3294bd11c984b5d3
seonghyeok <chosh2001@naver.com>
2025-08-14 16:33:04 +0900

    chore: 영업 시간 응답이 빈 경우 처리 추가

--- src/main/java/com/ll/commars/domain/restaurant/businessHour/dto/BusinessHourListResponse.java
+++ src/main/java/com/ll/commars/domain/restaurant/businessHour/dto/BusinessHourListResponse.java
@@ -1,6 +1,8 @@
 package com.ll.commars.domain.restaurant.businessHour.dto;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import com.ll.commars.domain.restaurant.businessHour.entity.BusinessHour;
 
@@ -9,10 +11,12 @@ public record BusinessHourListResponse(
 ) {
 
 	public static BusinessHourListResponse from(List<BusinessHour> businessHours) {
-		return new BusinessHourListResponse(
-			businessHours.stream()
-				.map(BusinessHourResponse::from)
-				.toList()
-		);
+		List<BusinessHourResponse> businessHourResponses = Optional.ofNullable(businessHours)
+			.orElse(Collections.emptyList())
+			.stream()
+			.map(BusinessHourResponse::from)
+			.toList();
+
+		return new BusinessHourListResponse(businessHourResponses);
 	}
 }
