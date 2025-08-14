fe41b01fa218b99f7b9e147857f280049ada0afe
seonghyeok <chosh2001@naver.com>
2025-08-14 15:46:16 +0900

    chore: 메뉴 생성 및 수정 DTO 검증 추가

--- src/main/java/com/ll/commars/domain/restaurant/menu/dto/MenuUpdateRequest.java
+++ src/main/java/com/ll/commars/domain/restaurant/menu/dto/MenuUpdateRequest.java
@@ -1,8 +1,14 @@
 package com.ll.commars.domain.restaurant.menu.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record MenuUpdateRequest(
+
+	@NotBlank(message = "메뉴 이름은 필수입니다.")
 	String menuName,
 	String imageUrl,
+
+	@NotBlank(message = "메뉴 가격은 필수입니다.")
 	Integer price
 ) {
 }
