c601ffda2783a8736761e87893062b56e5ccc42e
seonghyeok <chosh2001@naver.com>
2025-08-15 09:55:53 +0900

    feat: 특정 카테고리에 속하는 식당 조회 구현

--- src/main/java/com/ll/commars/domain/restaurant/restaurant/repository/jpa/RestaurantRepositoryCustom.java
+++ src/main/java/com/ll/commars/domain/restaurant/restaurant/repository/jpa/RestaurantRepositoryCustom.java
@@ -3,8 +3,10 @@ package com.ll.commars.domain.restaurant.restaurant.repository.jpa;
 import java.util.List;
 
 import com.ll.commars.domain.restaurant.restaurant.dto.RestaurantSummaryResponse;
+import com.ll.commars.domain.restaurant.restaurant.entity.RestaurantCategory;
 
 public interface RestaurantRepositoryCustom {
 
 	List<RestaurantSummaryResponse> findAllSummary();
+	List<RestaurantSummaryResponse> findByCategorySummary(RestaurantCategory category);
 }
