126ba6182c3eb97415433efcb735acfc5c1945d5
seonghyeok <chosh2001@naver.com>
2025-08-17 17:50:34 +0900

    refactor: docker 개발환경 세팅 분리 외
    - es-setup 스크립트 권한을 빌드 시 부여하도록

--- docker-infra/local/elasticsearch-setup/es-setup.sh
+++ docker-infra/local/elasticsearch-setup/es-setup.sh
@@ -10,7 +10,6 @@ done
 echo "Elasticsearch is up!"
 
 # 인덱스 템플릿 생성
-# URL과 옵션을 명확히 구분하여 가독성 향상
 curl -X PUT "http://elasticsearch:9200/_index_template/restaurants_template" \
      -H "Content-Type: application/json" \
      -d "@/tmp/restaurants_template.json"
