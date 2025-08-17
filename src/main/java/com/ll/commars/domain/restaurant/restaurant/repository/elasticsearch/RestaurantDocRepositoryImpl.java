3ba79563ddec6826e9db4647924fec25d5891966
seonghyeok <chosh2001@naver.com>
2025-08-17 17:04:13 +0900

    refactor: searchByKeywordAndLocation 리팩터링
    - 쿼리 컴포넌트 분리하여 가독성 높임

--- src/main/java/com/ll/commars/domain/restaurant/restaurant/repository/elasticsearch/RestaurantDocRepositoryImpl.java
+++ src/main/java/com/ll/commars/domain/restaurant/restaurant/repository/elasticsearch/RestaurantDocRepositoryImpl.java
@@ -13,8 +13,11 @@ import com.ll.commars.domain.restaurant.restaurant.entity.RestaurantDoc;
 
 import co.elastic.clients.elasticsearch._types.DistanceUnit;
 import co.elastic.clients.elasticsearch._types.SortOrder;
+import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
 import co.elastic.clients.elasticsearch._types.query_dsl.FieldValueFactorModifier;
 import co.elastic.clients.elasticsearch._types.query_dsl.FunctionBoostMode;
+import co.elastic.clients.elasticsearch._types.query_dsl.GeoDistanceQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.MultiMatchQuery;
 import co.elastic.clients.elasticsearch._types.query_dsl.Operator;
 import lombok.RequiredArgsConstructor;
 
@@ -26,30 +29,33 @@ public class RestaurantDocRepositoryImpl implements RestaurantDocRepositoryCusto
 
     @Override
     public List<RestaurantDoc> searchByKeywordAndLocation(String keyword, GeoPoint location, double distance) {
+        // 키워드 검색 쿼리
+        MultiMatchQuery textQuery = MultiMatchQuery.of(m -> m
+            .query(keyword)
+            .fields("name^2", "details")
+            .operator(Operator.Or)
+            .fuzziness("AUTO")
+        );
+
+        // 거리 필터링 쿼리
+        GeoDistanceQuery geoFilter = GeoDistanceQuery.of(g -> g
+            .field("location")
+            .location(l -> l.latlon(d -> d.lat(location.getLat()).lon(location.getLon())))
+            .distance(String.format("%.0fm", distance))
+        );
+
+        // 최종 쿼리
+        BoolQuery boolQuery = BoolQuery.of(b -> b
+            .must(textQuery._toQuery())
+            .filter(geoFilter._toQuery())
+        );
+
         Query query = NativeQuery.builder()
             .withQuery(q -> q
                 .functionScore(fs -> fs
-                    .query(
-                        b -> b.bool(bl -> bl
-                            .must(m -> m
-                                .multiMatch(mt -> mt
-                                    .query(keyword)
-                                    .fields("name^2", "details")
-                                    .operator(Operator.Or)
-                                    .fuzziness("AUTO")
-                                )
-                            )
-                            .filter(f -> f
-                                .geoDistance(g -> g
-                                    .field("location")
-                                    .location(l -> l.latlon(d -> d.lat(location.getLat()).lon(location.getLon())))
-                                    .distance(String.format("%.0fm", distance))
-                                )
-                            )
-                        )
-                    )
-                    .functions(
-                        f -> f.fieldValueFactor(fv -> fv
+                    .query(boolQuery._toQuery())
+                    .functions(f -> f
+                        .fieldValueFactor(fv -> fv
                             .field("average_rate")
                             .factor(1.5)
                             .modifier(FieldValueFactorModifier.Sqrt)
