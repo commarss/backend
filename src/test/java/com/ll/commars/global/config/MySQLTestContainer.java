41479a9a38919dc81148a7fa837d58da1fff38f1
seonghyeok <chosh2001@naver.com>
2025-08-18 11:01:38 +0900

    refactor: MySQLTestContainer 리팩터링
    - 매직 넘버를 상수로 분리
    - 헬퍼 메서드로 초기화

--- src/test/java/com/ll/commars/global/config/MySQLTestContainer.java
+++ src/test/java/com/ll/commars/global/config/MySQLTestContainer.java
@@ -8,11 +8,22 @@ import org.testcontainers.utility.DockerImageName;
 
 public class MySQLTestContainer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
-	private static final MySQLContainer<?> MYSQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
-		.withReuse(true);
+	private static final String MYSQL_IMAGE_NAME = "mysql:8.0";
+	private static final String DATABASE_NAME = "commars_test";
+	private static final String MYSQL_USERNAME = "testuser";
+	private static final String MYSQL_PASSWORD = "testpassword";
+	private static final MySQLContainer<?> MYSQL_CONTAINER = createAndStartContainer();
 
-	static {
-		MYSQL_CONTAINER.start();
+	private static MySQLContainer<?> createAndStartContainer() {
+		MySQLContainer<?> container = new MySQLContainer<>(DockerImageName.parse(MYSQL_IMAGE_NAME))
+			.withDatabaseName(DATABASE_NAME)
+			.withUsername(MYSQL_USERNAME)
+			.withPassword(MYSQL_PASSWORD)
+			.withReuse(true);
+
+		container.start();
+
+		return container;
 	}
 
 	@Override
