b332b2d8dbfd495c2658e3f4b8423bd12b65091c
seonghyeok <chosh2001@naver.com>
2025-08-18 19:38:53 +0900

    feat: community 도메인에서 AuthMemberId 어노테이션을 통해 사용자 정보를 가져오도록

--- src/main/java/com/ll/commars/domain/community/comment/controller/CommentController.java
+++ src/main/java/com/ll/commars/domain/community/comment/controller/CommentController.java
@@ -1,8 +1,6 @@
 package com.ll.commars.domain.community.comment.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +14,7 @@ import com.ll.commars.domain.community.comment.dto.CommentCreateResponse;
 import com.ll.commars.domain.community.comment.dto.CommentUpdateRequest;
 import com.ll.commars.domain.community.comment.dto.CommentUpdateResponse;
 import com.ll.commars.domain.community.comment.service.CommentService;
-import com.ll.commars.domain.member.entity.Member;
-import com.ll.commars.domain.member.service.MemberService;
+import com.ll.commars.global.security.annotation.AuthMemberId;
 
 import lombok.RequiredArgsConstructor;
 
@@ -27,20 +24,13 @@ import lombok.RequiredArgsConstructor;
 public class CommentController {
 
 	private final CommentService commentService;
-	private final MemberService memberService;
-
-	private Member getAuthenticatedUser(@AuthenticationPrincipal UserDetails userDetails) {
-		return memberService.findById(Long.parseLong(userDetails.getUsername())).orElseThrow(
-			() -> new RuntimeException("사용자를 찾을 수 없습니다."));
-	}
 
 	@PostMapping
 	public ResponseEntity<CommentCreateResponse> createComment(
-		@RequestBody CommentCreateRequest request,
-		@AuthenticationPrincipal UserDetails userDetails
+		@AuthMemberId long memberId,
+		@RequestBody CommentCreateRequest request
 	) {
-		Member member = getAuthenticatedUser(userDetails);
-		CommentCreateResponse response = commentService.createComment(member.getId(), request);
+		CommentCreateResponse response = commentService.createComment(memberId, request);
 
 		return ResponseEntity.ok(response);
 	}
@@ -48,11 +38,10 @@ public class CommentController {
 	@PatchMapping("/{comment-id}")
 	public ResponseEntity<CommentUpdateResponse> updateComment(
 		@PathVariable("comment-id") Long commentId,
-		@RequestBody CommentUpdateRequest request,
-		@AuthenticationPrincipal UserDetails userDetails
+		@AuthMemberId long memberId,
+		@RequestBody CommentUpdateRequest request
 	) {
-		Member member = getAuthenticatedUser(userDetails);
-		CommentUpdateResponse response = commentService.updateComment(member.getId(), commentId, request);
+		CommentUpdateResponse response = commentService.updateComment(memberId, commentId, request);
 
 		return ResponseEntity.ok(response);
 	}
@@ -60,10 +49,9 @@ public class CommentController {
 	@DeleteMapping("/{comment-id}")
 	public ResponseEntity<Void> deleteComment(
 		@PathVariable("comment-id") Long commentId,
-		@AuthenticationPrincipal UserDetails userDetails
+		@AuthMemberId long memberId
 	) {
-		Member member = getAuthenticatedUser(userDetails);
-		commentService.deleteComment(commentId, member.getId());
+		commentService.deleteComment(commentId, memberId);
 
 		return ResponseEntity.ok().build();
 	}
