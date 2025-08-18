14f267823f851e2df0fe2c5f5ac9f64ef9970b3d
seonghyeok <chosh2001@naver.com>
2025-08-18 11:09:36 +0900

    refactor: RedisTestContainer 리팩터링
    - 매직 넘버를 상수로 분리
    - 헬퍼 메서드로 초기화

--- src/test/java/com/ll/commars/global/config/RedisTestContainer.java
+++ src/test/java/com/ll/commars/global/config/RedisTestContainer.java
@@ -9,16 +9,19 @@ import org.testcontainers.utility.DockerImageName;
 
 public class RedisTestContainer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
-	private static final int REDIS_PORT = 6379;
 	private static final String REDIS_DOCKER_IMAGE = "redis:7-alpine";
-	private static final GenericContainer<?> REDIS_CONTAINER;
+	private static final int REDIS_PORT = 6379;
+	private static final GenericContainer<?> REDIS_CONTAINER = createAndStartContainer();
 
-	static {
-		REDIS_CONTAINER = new GenericContainer<>(DockerImageName.parse(REDIS_DOCKER_IMAGE))
+	private static GenericContainer<?> createAndStartContainer() {
+		GenericContainer<?> container = new GenericContainer<>(DockerImageName.parse(REDIS_DOCKER_IMAGE))
 			.withExposedPorts(REDIS_PORT)
 			.waitingFor(Wait.forListeningPort())
-			.withReuse(true); // todo: CI 환경에서 불안정하게 동작할 수 있음
-		REDIS_CONTAINER.start();
+			.withReuse(true);
+
+		container.start();
+
+		return container;
 	}
 
 	@Override
