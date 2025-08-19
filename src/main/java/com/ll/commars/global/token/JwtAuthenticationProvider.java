ae0c9a1b281629b08bc63a1d9aa6df31652dd6eb
seonghyeok <chosh2001@naver.com>
2025-08-19 22:20:47 +0900

    fix: 로그아웃 시 이전 토큰을 통해 인가가 필요한 리소스에 접근하지 못하도록

--- src/main/java/com/ll/commars/global/token/JwtAuthenticationProvider.java
+++ src/main/java/com/ll/commars/global/token/JwtAuthenticationProvider.java
@@ -1,10 +1,13 @@
 package com.ll.commars.global.token;
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 
 import com.ll.commars.global.security.service.CustomUserDetailsService;
 import com.ll.commars.global.token.component.JwtProvider;
@@ -20,10 +23,17 @@ public class JwtAuthenticationProvider implements AuthenticationProvider {
 
 	private final JwtProvider jwtProvider;
 	private final CustomUserDetailsService customUserDetailsService;
+	private final RedisTemplate<String, String> redisTemplate;
 
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 		String accessToken = (String) authentication.getPrincipal();
+
+		Object isBlacklisted = redisTemplate.opsForValue().get(accessToken);
+		if (!ObjectUtils.isEmpty(isBlacklisted)) {
+			throw new BadCredentialsException("로그아웃된 토큰입니다. 다시 로그인해주세요.");
+		}
+
 		JwtClaims claims = jwtProvider.parseClaim(TokenValue.of(accessToken));
 		UserDetails userDetails = customUserDetailsService.loadUserByUsername(claims.publicClaims().subject().value());
 
